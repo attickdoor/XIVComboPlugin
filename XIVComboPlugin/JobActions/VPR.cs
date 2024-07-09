f552816c16f5e0f46cafcde44279488da7a37884
unknown <warlek@yahoo.ca>
2024-07-08 21:24:24 -0400

    start

---
+++ XIVComboPlugin/JobActions/VPR.cs
@@ -0,0 +1,19 @@
+﻿namespace XIVComboPlugin.JobActions
+{
+    public static class VPR
+    {
+        public const uint
+            StormsPath = 42,
+            HeavySwing = 31,
+            Maim = 37,
+            StormsEye = 45,
+            MythrilTempest = 16462,
+            Overpower = 41,
+            InnerRelease = 7389,
+            PrimalRend = 25753,
+            Berserk = 38;
+
+        public const ushort
+            BuffPrimalRendReady = 2624;
+    }
+}

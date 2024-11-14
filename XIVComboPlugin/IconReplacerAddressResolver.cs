8a50de41c2fdb38eaeb45bebe84fdc64614db5f3
Pine <eli.stonium@gmail.com>
2024-11-14 09:25:54 -0500

    7.1 Support

--- XIVComboPlugin/IconReplacerAddressResolver.cs
+++ XIVComboPlugin/IconReplacerAddressResolver.cs
@@ -11,7 +11,7 @@ namespace XIVComboPlugin
 
         public IconReplacerAddressResolver(ISigScanner sig)
         {
-            this.IsIconReplaceable = sig.ScanText("40 53 48 83 EC 20 8B D9 48 8B 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 85 C0 74 1F");
+            this.IsIconReplaceable = sig.ScanText("40 53 48 83 EC 20 8B D9 48 8B 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 85 C0 74 1B");
         }
     }
 }
